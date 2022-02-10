1fe817ecdebd16d3aabe12f8c673f543bcf2fa60
tu.dinh <tu.dinh@metropolia.fi>
2022-02-10 02:10:30 +0200

    build all the needed components and simulate application after search result

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,25 +1,19 @@
 import logo from '/Users/tudinh/ravenous/src/logo.svg';
+import React from 'react';
 import './App.css';
+import BusinessList from '../BusinessList/BusinessList';
+import SearchBar from '../SearchBar/SearchBar';
 
-function App() {
-  return (
+class App extends React.Component {
+  render() {
+    return(
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello Codecademy!
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <h1>ravenous</h1>
+        <SearchBar/>
+        <BusinessList/>
+  </div>
+  )
+  }
 }
 
 export default App;
