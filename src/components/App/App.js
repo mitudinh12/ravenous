e243c1447f2dcfb8cc4a2c6c212576aac05543f0
tu.dinh <tu.dinh@metropolia.fi>
2022-02-15 01:22:04 +0200

    set state for searchBar elements and simulate a search query

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,11 +18,15 @@ const business = {
 
 const businesses = [];
 class App extends React.Component {
+  searchYelp(term, location, sortBy) {
+    console.log(`Searching Yelp with ${term}, ${location}, ${sortBy}`);
+  }
+
   render() {
     return(
     <div className="App">
       <h1>ravenous</h1>
-        <SearchBar/>
+        <SearchBar searchResult={this.searchYelp}/>
         <BusinessList businesses={businesses}/>
   </div>
   )
